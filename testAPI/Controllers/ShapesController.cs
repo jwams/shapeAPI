9a3359e20ec665c3716460878b6aa3e7749d4bd2
Justin Wams <25326784+jwams@users.noreply.github.com>
2019-11-20 08:49:00 -0800

    Initial Commit

---
+++ testAPI/Controllers/ShapesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using testAPI.Models;
+
+namespace testAPI.Controllers
+{
+    public class ShapesController : ApiController
+    {
+        Shape[] shapes = new Shape[] {
+            new Shape { Id = 1, Name = "Square"},
+            new Shape { Id = 2, Name = "Circle"},
+            new Shape { Id = 3, Name = "Rectangle"}
+        };
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var shape = shapes.FirstOrDefault((p) => p.Id == id);
+            if (shape == null)
+            {
+                return NotFound();
+            }
+            return Ok(shape);
+        }
+    }
+}
---
+++ testAPI/Models/Shape.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace testAPI.Models
+{
+    public class Shape
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
